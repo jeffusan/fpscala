b5b361ab5c528de798e57de80f8a0e853479f81f
Jeff Hemminger <jeff.hemminger@deciphernow.com>
2019-03-04 18:05:46 -0500

    added tests

--- src/test/scala/chapter5/StreamSpec.scala
+++ src/test/scala/chapter5/StreamSpec.scala
@@ -7,28 +7,24 @@ class StreamSpec extends PropSpec with Matchers with PropertyChecks {
 
   val ten = Stream(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-  property("Exercise 1 - Write a function to convert a Stream to a List") {
+  property("Exercise 1 - convert a Stream to a List") {
     ten.toList shouldBe (1 to 10).toList
   }
 
   property(
-    "Exercise 2 - Write a function take for returning the first n elements of a Stream") {
+    "Exercise 2 - take the first n elements of a Stream") {
     ten.take(3).toList shouldBe (1 to 3).toList
   }
 
   property(
-    "Exercise 3 - Write the function takeWhile for returning all staring elements of a Stream that match a given predicate") {
+    "Exercise 3 - takeWhile all elements of a Stream while match") {
     ten.takeWhile(_ <= 5).toList shouldBe (1 to 5).toList
   }
 
   property(
-    "Exercise 4 - Implement forAll, which checks that all elements in the Stream match a given predicate.") {
+    "Exercise 4 - all elements in the Stream match a given predicate.") {
     ten.forAll(_ <= 10) shouldBe true
     Stream(1, 2, 3, 4, 5).forAll(_ <= 3) shouldBe false
-  }
-
-  property("Exercise 5 - Use foldRight to implement takeWhile") {
-    ten.takeWhile(_ <= 5).toList shouldBe (1 to 5).toList
   }
 
   property("Exercise 6 - Implement map, filter, append, and flatMap by using foldRight") {
@@ -39,5 +35,25 @@ class StreamSpec extends PropSpec with Matchers with PropertyChecks {
     ten.append(Stream(11, 12, 13)).toList shouldBe (1 to 13)
 
     ten.flatMap((x: Int) => Stream(x + 1)).toList shouldBe (2 to 11)
+
+    ten.headOption shouldBe Some(1)
+
+    Stream().headOption shouldBe None
+  }
+
+  property("Exercise 8- return constant value repeatedly") {
+    Stream.constant("aloha").take(3).toList shouldBe List("aloha","aloha","aloha")
+  }
+
+  property("Exercise 9- return an infinite range") {
+    Stream.from(1).take(10).toList shouldBe (1 to 10)
+  }
+
+  property("Exercise 10 - generate fibs") {
+    Stream.fibs().take(10).toList shouldBe List(0,1,1,2,3,5,8,13,21,34)
+  }
+
+  property("Exercise 16- scan right") {
+    Stream(1,2,3).scanRight(0)(_ + _).toList shouldBe List(6,5,3,0)
   }
 }
