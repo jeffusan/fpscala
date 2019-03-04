c5534c913d3f1ac71e6ec7597d926f6a22ba8e62
Jeff Hemminger <jeff.hemminger@gmail.com>
2019-03-03 20:51:42 -0500

    a few fewer brackets

--- src/main/scala/chapter5/Stream.scala
+++ src/main/scala/chapter5/Stream.scala
@@ -48,45 +48,40 @@ trait Stream[+A] {
 
   def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(Stream.empty[B])((h, t) => f(h).append(t))
 
-  def zipWith[B, C](s2: Stream[B])(f: (A, B) => C): Stream[C] = {
-    unfold((this, s2)) {
+  def zipWith[B, C](s2: Stream[B])(f: (A, B) => C): Stream[C] = unfold((this, s2)) {
       case (Cons(h1, t1), Cons(h2, t2)) => Some((f(h1(), h2()), (t1(), t2())))
       case _ => None
     }
-  }
 
-  def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] = {
-    unfold(this, s2) {
+
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] = unfold(this, s2) {
       case (Cons(h1, t1), Empty) => Some((Some(h1()), None), (t1(), Stream.empty))
       case (Empty, Cons(h2, t2)) => Some((None, Some(h2())), (Stream.empty, t2()))
       case (Cons(h1, t1), Cons(h2, t2)) => Some((Some(h1()), Some(h2())), (t1(), t2()))
       case _ => None
     }
-  }
 
-  def startsWith[B](s: Stream[B]): Boolean = {
-    unfold(this, s) {
+
+  def startsWith[B](s: Stream[B]): Boolean = unfold(this, s) {
       case (Empty, Cons(_, _)) => Some(false, (Stream.empty, Stream.empty))
       case (Cons(h1, t1), Cons(h2, t2)) => Some(h1() == h2(), (t1(), t2()))
       case _ => None
     }.forAll(a => a)
-  }
 
-  def tails: Stream[Stream[A]] = {
-    unfold(this) {
+
+  def tails: Stream[Stream[A]] = unfold(this) {
       case Empty => None
       case Cons(h, t) => Some((Stream.cons(h(), t()), t()))
     }.append(Stream(Stream.empty))
-  }
 
-  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] = {
-    foldRight(Stream.empty[B])((h1, t1) => {
+
+  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] = foldRight(Stream.empty[B])((h1, t1) => {
       t1 match {
         case Empty => Stream(f(h1, z)).append(Stream(z))
         case Cons(h2, _) => Stream.cons(f(h1, h2()), t1)
       }
     })
-  }
+
 }
 
 object Stream {
